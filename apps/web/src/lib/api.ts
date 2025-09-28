f3be3dd2d6f1e95c383ef32532ef3748ab6bad22
adityaadpandey <adityapandeyadp@gmail.com>
2025-09-28 22:16:10 +0530

    next js build error fixed

--- apps/web/src/lib/api.ts
+++ apps/web/src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://alphaevents.sevalla.app/api/v1",
+  baseURL: "https://api.tixin.in/api/v1",
   headers: { "Content-Type": "application/json" },
 });
 
