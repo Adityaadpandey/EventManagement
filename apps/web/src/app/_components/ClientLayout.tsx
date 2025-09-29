b8a2f0451dc385b100ab84198cac4bd6f99d7cbb
Rohit-Singh-07 <rohitsingh234@lpu.in>
2025-09-29 07:58:25 +0530

    update layout and nav

---
+++ apps/web/src/app/_components/ClientLayout.tsx
@@ -0,0 +1,27 @@
+// src/app/_components/ClientLayout.tsx
+"use client";
+
+import { usePathname } from "next/navigation";
+import NavBar from "./Navbar";
+import React from "react";
+
+export default function ClientLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const pathname = usePathname();
+
+  // Add more routes to this array if needed
+  const hideNavOnRoutes = ["/event"];
+  const shouldHideNav = hideNavOnRoutes.some((path) =>
+    pathname.startsWith(path),
+  );
+
+  return (
+    <main className="flex flex-col h-screen relative overflow-auto">
+      {children}
+      {!shouldHideNav && <NavBar />}
+    </main>
+  );
+}
