75a0b191db4471cd6d161f8f37d47a54ab32b24f
adityaadpandey <adityapandeyadp@gmail.com>
2025-10-08 23:05:47 +0530

    email service updated to aws and template changes

--- servers/user-server/src/config/index.ts
+++ servers/user-server/src/config/index.ts
@@ -17,8 +17,10 @@ interface Config {
   RAZORPAY_KEY_SECRET: string;
   RAZORPAY_WEBHOOK_SECRET: string;
 
-  SMTP_EMAIL_USER: string;
-  SMTP_EMAIL_PASS: string;
+  SMTP_HOST: string;
+  SMTP_PORT: number;
+  SMTP_USER: string;
+  SMTP_PASS: string;
 }
 
 export const config: Config = {
@@ -35,6 +37,10 @@ export const config: Config = {
   RAZORPAY_KEY_ID: process.env.RAZORPAY_KEY_ID || "",
   RAZORPAY_KEY_SECRET: process.env.RAZORPAY_KEY_SECRET || "",
   RAZORPAY_WEBHOOK_SECRET: process.env.RAZORPAY_WEBHOOK_SECRET || "",
-  SMTP_EMAIL_USER: process.env.SMTP_EMAIL_USER || "",
-  SMTP_EMAIL_PASS: process.env.SMTP_EMAIL_PASS || "",
+  SMTP_HOST: process.env.SMTP_HOST || "",
+  SMTP_PORT: process.env.SMTP_PORT ? parseInt(process.env.SMTP_PORT, 10) : 587,
+  SMTP_USER: process.env.SMTP_USER || "",
+  SMTP_PASS: process.env.SMTP_PASS || "",
+  // SMTP_EMAIL_USER: process.env.SMTP_EMAIL_USER || "",
+  // SMTP_EMAIL_PASS: process.env.SMTP_EMAIL_PASS || "",
 };
