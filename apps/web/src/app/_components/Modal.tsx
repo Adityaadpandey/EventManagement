0179ab852799892f452dd93c6c879a68a0933b9e
Rohit-Singh-07 <rohitsingh234@lpu.in>
2025-10-30 21:18:21 +0530

    update discount pricing

--- apps/web/src/app/_components/Modal.tsx
+++ apps/web/src/app/_components/Modal.tsx
@@ -785,10 +785,43 @@ const Modal: React.FC<ModalProps> = ({
                           <h5 className="text-[#8B8B8B]">TOTAL</h5>
                           <h5>
                             ₹
-                            {(
-                              (selectedTicket?.price ?? 0) * selectedQuantity +
-                              selectedTicket.platformfee * selectedQuantity
-                            ).toFixed(2)}
+                            {selectedTicket?.discountedPrice ? (
+                              <>
+                                <span className="discounted-price">
+                                  ₹
+                                  {(
+                                    (selectedTicket.discountedPrice ?? 0) *
+                                      selectedQuantity +
+                                    selectedTicket.platformfee *
+                                      selectedQuantity
+                                  ).toFixed(2)}
+                                </span>
+                                <span
+                                  className="original-price"
+                                  style={{
+                                    textDecoration: "line-through",
+                                    marginLeft: "8px",
+                                  }}
+                                >
+                                  ₹
+                                  {(
+                                    (selectedTicket.price ?? 0) *
+                                      selectedQuantity +
+                                    selectedTicket.platformfee *
+                                      selectedQuantity
+                                  ).toFixed(2)}
+                                </span>
+                              </>
+                            ) : (
+                              <span>
+                                ₹
+                                {(
+                                  (selectedTicket?.price ?? 0) *
+                                    selectedQuantity +
+                                  selectedTicket.platformfee * selectedQuantity
+                                ).toFixed(2)}
+                              </span>
+                            )}
                           </h5>
                         </div>
                       </div>
