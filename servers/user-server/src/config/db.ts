3d155d291f32cb1257df52bce914e816d69ba1c0
adityaadpandey <adityapandeyadp@gmail.com>
2025-11-23 22:51:56 +0530

    migration to prisma 7

--- servers/user-server/src/config/db.ts
+++ servers/user-server/src/config/db.ts
@@ -1,10 +1,9 @@
 import "dotenv/config";
 
 import { PrismaPg } from "@prisma/adapter-pg";
-import { config } from ".";
 import { PrismaClient } from "../generated/prisma/client";
 
-const connectionString = `${config.DATABASE_URL}`;
+const connectionString = `${process.env.DATABASE_URL}`;
 
 const adapter = new PrismaPg({ connectionString });
 const prisma = new PrismaClient({ adapter });
