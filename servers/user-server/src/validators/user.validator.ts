d4c61baedf2e8c3c23e635789f9402bec115ba37
adityaadpandey <adityapandeyadp@gmail.com>
2026-01-25 15:03:46 +0530

    build fixes

--- servers/user-server/src/validators/user.validator.ts
+++ servers/user-server/src/validators/user.validator.ts
@@ -1,14 +1,8 @@
 import { z } from "zod";
 
-export const updateUserProfileSchema = z
-  .object({
-    name: z.string().min(1, "Name cannot be empty").optional(),
-    email: z.string().email("Invalid email address").optional(),
-    avatar: z.string().url("Invalid avatar URL").optional(),
-    phone: z.string().min(13, "Invalid phone number").optional(),
-  })
-
-  .refine((data) => data.name || data.email || data.avatar || data.phone, {
-    message:
-      "At least one field (name, email, avatar, phone) is required to update",
-  });
+export const updateUserProfileSchema = z.object({
+  name: z.string().min(1, "Name cannot be empty").optional(),
+  email: z.string().email("Invalid email address").optional(),
+  avatar: z.string().url("Invalid avatar URL").optional(),
+  phone: z.string().min(13, "Invalid phone number").optional(),
+});
