1c322b1199e065c6b10d9562a355dc68d49e58d2
adityaadpandey <adityapandeyadp@gmail.com>
2026-02-04 18:56:16 +0530

    feat(jobs): add subscription cleanup cron job
    Co-Authored-By: Aditya Pandey <adityapandeyadp@gmail.com>

--- servers/user-server/src/index.ts
+++ servers/user-server/src/index.ts
@@ -1,5 +1,7 @@
 import { startServer } from "./app";
+import { startSubscriptionCleanupJob } from "./jobs/cleanup-subscriptions.job";
 import { startEventReminderJob } from "./jobs/schedule-event-reminders.job";
 
 startServer();
 startEventReminderJob();
+startSubscriptionCleanupJob();
