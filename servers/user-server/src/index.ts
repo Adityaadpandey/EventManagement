54ebc6fbb94f4c8c7936080b959866a43f5521f2
adityaadpandey <adityapandeyadp@gmail.com>
2026-02-04 18:58:22 +0530

    feat(worker): start notification worker on server boot
    Co-Authored-By: Aditya Pandey <adityapandeyadp@gmail.com>

--- servers/user-server/src/index.ts
+++ servers/user-server/src/index.ts
@@ -1,6 +1,7 @@
 import { startServer } from "./app";
 import { startSubscriptionCleanupJob } from "./jobs/cleanup-subscriptions.job";
 import { startEventReminderJob } from "./jobs/schedule-event-reminders.job";
+import "./workers/notification.worker";
 
 startServer();
 startEventReminderJob();
